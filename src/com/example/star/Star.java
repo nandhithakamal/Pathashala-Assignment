a7ead87820428c4b86174615692462b169f0c6fc
Nandhitha Kamal <nandhithakml@gmail.com>
2018-06-11 22:04:16 +0530

    Completed task 1 - printing a star

---
+++ src/com/example/star/Star.java
@@ -0,0 +1,9 @@
+package com.example.star;
+
+public class Star {
+   public void printStar(){
+       System.out.println("*");
+    }
+
+}
+
---
+++ src/com/example/star/StarTestDrive.java
@@ -0,0 +1,8 @@
+package com.example.star;
+
+public class StarTestDrive{
+    public static void main(String[] args) {
+        Star s = new Star();
+        s.printStar();
+    }
+}
